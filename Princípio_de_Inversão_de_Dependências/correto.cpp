235f1e3f47760c54817151ff624b06b2950a65ea
Carolina Fujii <carolinayumi89@gmail.com>
2024-04-29 20:06:53 -0300

    Princípio de Inversão de Dependências

---
+++ Princípio_de_Inversão_de_Dependências/correto.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+/*Define um contrato comum para todos os serviços de mensagem: */
+class MessageService {
+public:
+    virtual void sendMessage(const std::string& recipient, const std::string& message) = 0;
+    virtual ~MessageService() {} // Destructor virtual
+};
+
+class EmailService : public MessageService {
+public:
+    void sendMessage(const std::string& recipient, const std::string& message) override {
+        std::cout << "Sending email to " << recipient << ": " << message << std::endl;
+    }
+};
+
+class SMSService : public MessageService {
+public:
+    void sendMessage(const std::string& phoneNumber, const std::string& message) override {
+        std::cout << "Sending SMS to " << phoneNumber << ": " << message << std::endl;
+    }
+};
+
+class NotificationService {
+private:
+    MessageService& messageService;
+public:
+    NotificationService(MessageService& service) : messageService(service) {}
+
+    void sendNotification(const std::string& recipient, const std::string& message) {
+        messageService.sendMessage(recipient, message);
+    }
+};
---
+++ Princípio_de_Inversão_de_Dependências/errado.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class EmailService {
+public:
+    void sendEmail(const std::string& recipient, const std::string& message) {
+        std::cout << "Sending email to " << recipient << ": " << message << std::endl;
+    }
+};
+
+class SMSService {
+public:
+    void sendSMS(const std::string& phoneNumber, const std::string& message) {
+        std::cout << "Sending SMS to " << phoneNumber << ": " << message << std::endl;
+    }
+};
+
+/*Este exemplo viola o principio porque a classe NotificationService
+ está diretamente ligada às implementações concretas EmailService e SMSService. */
+class NotificationService {
+private:
+    EmailService emailService;
+    SMSService smsService;
+public:
+    void sendNotification(const std::string& recipient, const std::string& message, bool isEmail) {
+        if (isEmail) {
+            emailService.sendEmail(recipient, message);
+        } else {
+            smsService.sendSMS(recipient, message);
+        }
+    }
+};
